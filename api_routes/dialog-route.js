4e5001572f9fd8331da52cb3657871983430d565
Elly Kitoto <ekitoto@ona.io>
2018-11-19 15:38:05 +0300

    Added dialog route

---
+++ api_routes/dialog-route.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const DialogRouter = express.Router()
+
+//Handle post request from slack
+DialogRouter.post('/dialog/new', function (req, res, next) {
+    
+    res.statusCode = 200
+    res.send("{}")
+})
+
+//Test get request from slack
+DialogRouter.get('/dialog', function (req, res, next) {
+    res.send("Cool! Everything works for dialog! Congratulations!!")
+})
+
+module.exports = DialogRouter
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const SlashCommandRouter = require("./api_routes/slash-command-route.js")
+const DialogRouter = require("./api_routes/dialog-route.js")
 const AutoStandup = require("./slack-bot")
 const listeningPort = "port"
 const ontime = require("ontime")
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api/v1", SlashCommandRouter)
+app.use("/api/v1", DialogRouter)
 
 // Error handling middleware
 app.use(function (err, req, res, next) {
