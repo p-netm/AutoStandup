4a4c78abfb79b3988d3a22dd2cccee78724c139c
Elly Kitoto <ekitoto@ona.io>
2019-03-04 15:20:00 +0300

    Included PM2 configuration in docker file

--- standup.js
+++ standup.js
@@ -42,6 +42,6 @@ app.get('/', (req, res) => {
 appBootstrap.main();
 onTimeService.runSchedules();
 
-app.listen(process.env.PORT || 7777, function () {
+app.listen(process.env.PORT || 8008, function () {
     console.log("[+] app listening for requests")
 });
